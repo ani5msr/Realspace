70017b7e20a0e7cf2c8a2a85d21e226bc1d18ba5
Anirudh Mishra <anirudh5msr@gmail.com>
2022-11-17 17:47:14 +0530

    Added search posts by email and userid

--- src/main/java/services/Postdb.java
+++ src/main/java/services/Postdb.java
@@ -49,4 +49,53 @@ public class Postdb {
 		 em.close();
 		 }
 		 }
+	public static List<Yspost> ysPost (){
+		 EntityManager em = Dbutil.getEntityManager
+		("Realspace");
+		 String qString = "select b from Yspost b";
+		 List<Yspost> posts = null;
+		 try{TypedQuery<Yspost> query = em.createQuery(qString,Yspost.class);
+		 posts = query.getResultList();
+		 }catch (Exception e){
+		 e.printStackTrace();
+		 }
+		 finally{
+		 em.close();
+		 }
+		 return posts;
+		 }
+	public static List<Yspost> postsofUser(long userid)
+	 {
+	 EntityManager em = Dbutil.getEntityManager("Realspace");
+	 List<Yspost> userposts = null;
+	 String qString = "select b from Yspost b where b.ysuser.ysuserid = :userid";
+	 try{
+	 TypedQuery<Yspost> query = em.createQuery(qString,Yspost.class);
+	 query.setParameter("userid", userid);
+	 userposts = query.getResultList();
+	 }catch (Exception e){
+	 e.printStackTrace();
+	 }
+	 finally{
+	 em.close();
+	 }
+	 return userposts;
+	 }
+	public static List<Yspost> postsofUser(String useremail)
+	 {
+	 EntityManager em = Dbutil.getEntityManager("Realspace");
+	 List<Yspost> userposts = null;
+	 String qString = "select b from Yspost b " + "where b.ysuser.useremail = :useremail";
+	 try{
+	 TypedQuery<Yspost> query = em.createQuery(qString,Yspost.class);
+	 query.setParameter("useremail",useremail);
+	 userposts = query.getResultList();
+	 }catch (Exception e){
+	 e.printStackTrace();
+	 }
+	 finally{
+	 em.close();
+	 }
+	 return userposts;
+	 }
 }
